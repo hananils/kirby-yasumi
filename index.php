4480d29cdf6af2be8779d90536efd316640aef2c
Nils Hörrmann <nils.hoerrmann@hananils.de>
2026-05-13 12:27:10 +0200

    Update formatting and types

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 
-@include_once __DIR__ . '/vendor/autoload.php';
+use Kirby\Cms\App as Kirby;
+use Kirby\Cms\Pages;
+use Yasumi\Yasumi;
+
+require_once __DIR__ . '/vendor/autoload.php';
 
 Kirby::plugin('hashsandsalt/yasumi', [
-
     'siteMethods' => [
-        'holidays' => function ($country, $year, $locale = 'en_US') {
-            $holidays = Yasumi\Yasumi::create($country, $year, $locale);
-            return $holidays;
+        'holidays' => function (
+            string $country,
+            int $year,
+            string $locale = 'en_US'
+        ) {
+            return Yasumi::create($country, $year, $locale);
         }
     ],
-
     'collections' => [
-        'holidays' => function ($site) {
+        'holidays' => function ($site): Pages {
             $country = option('hashandsalt.yasumi.country', 'UnitedKingdom');
-            $year = option('hashandsalt.yasumi.year', date('Y'));
+            $year = intval(option('hashandsalt.yasumi.year', date('Y')));
             $locale = option('hashandsalt.yasumi.locale', 'en_GB');
 
             foreach ($site->holidays($country, $year, $locale) as $holiday) {
@@ -35,5 +40,4 @@ Kirby::plugin('hashsandsalt/yasumi', [
             return Pages::factory($pages);
         }
     ]
-
 ]);
